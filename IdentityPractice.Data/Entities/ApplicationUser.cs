24ab049ace4620967652164cc7a9d2f92268a798
Máté Mózer <68518035+Sh4dizz@users.noreply.github.com>
2022-10-05 18:27:01 +0200

    inital

---
+++ IdentityPractice.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityPractice.Data.Entities
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+
+
+        public ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
---
+++ IdentityPractice.Data/Entities/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityPractice.Data.Entities
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public DateTime Year { get; set; }
+        public bool IsActive { get; set; }
+
+
+        public ApplicationUser owner { get; set; }
+    }
+}
